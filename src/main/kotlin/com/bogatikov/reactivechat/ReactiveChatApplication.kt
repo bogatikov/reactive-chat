7cf577e1d248883cf6495653da28b3325fb168d0
Антон Богатиков <13370803+bogatikov@users.noreply.github.com>
2021-04-13 18:03:00 +0300

    Remove unnecessary annotation

--- src/main/kotlin/com/bogatikov/reactivechat/ReactiveChatApplication.kt
+++ src/main/kotlin/com/bogatikov/reactivechat/ReactiveChatApplication.kt
@@ -2,15 +2,10 @@ package com.bogatikov.reactivechat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
-
 
 @SpringBootApplication
-@EnableScheduling
 class ReactiveChatApplication
 
 fun main(args: Array<String>) {
     runApplication<ReactiveChatApplication>(*args)
-
-
 }
